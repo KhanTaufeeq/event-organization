10fee3a09a2c95265fb283f0ef6a289e188bb244
KhanTaufeeq <taufeeqyouth@gmail.com>
2025-09-18 09:11:06 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,20 @@
+// const backgroundImg = document.querySelectorAll('.background-img');
+// console.log(backgroundImg);
+
+// for (let i = 0; i < backgroundImg.length; i++){
+//     setTimeout(() => {
+//         backgroundImg[i].style.display = 'block';
+//     }, 2000)
+// }
+
+// setTimeout(() => {
+//     for (let i = 0; i < backgroundImg.length; i++){
+//         backgroundImg[i].style.display = 'block';
+//     }
+// }, 2000)
+
+// setInterval(() => {
+//     for (let i = 0; i < backgroundImg.length; i++){
+//         backgroundImg[i].style.display = 'block';
+//      }
+// }, 2000)
